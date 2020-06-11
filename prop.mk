4b56cb231bd422bedd0b9a2763d5fd523ace5cb5
Alejandro <apamarin@gmail.com>
2020-06-11 08:32:17 +0200

    daisy: fix dual sim
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- prop.mk
+++ prop.mk
@@ -239,7 +239,8 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true \
 service.qti.ims.enabled=1 \
 persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
-persist.vendor.cne.logging.qxdm=3974
+persist.vendor.cne.logging.qxdm=3974 \
+ro.vendor.multisim.simslotcount=2
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
