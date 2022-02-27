56b4eae023183a31bf0ecab813df2c867106878c
lupesoltec <dev@lupesoltec.com.br>
2022-02-27 15:14:57 -0400

    daisy: Add AndroidAutoStubPrebuilt packages in runtime
    * This is required for AndroidAuto to work
    * List of all gapps: https://gitlab.com/havoc-gapps/android_vendor_gapps/-/blob/eleven/config.mk
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt SoundAmplifierPrebuilt arcore Drive GoogleCamera Ornament TagGoogle talkback YouTube YouTubeMusicPrebuilt DevicePolicyPrebuilt
+LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt arcore Drive GoogleCamera Ornament TagGoogle talkback YouTube YouTubeMusicPrebuilt DevicePolicyPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
