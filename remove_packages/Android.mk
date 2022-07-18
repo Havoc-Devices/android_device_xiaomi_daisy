5585e9738c2ec245a43c6829d5850df91906f7f5
lupesoltec <dev@lupesoltec.com.br>
2022-07-18 13:41:40 -0400

    daisy: Remove some packages in runtime for A12
    * our system partition crying
    * List of all gapps: https://gitlab.com/havoc-gapps/android_vendor_gapps/-/blob/eleven/config.mk
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SoundAmplifierPrebuilt arcore Drive GoogleCamera Ornament TagGoogle talkback YouTube YouTubeMusicPrebuilt DevicePolicyPrebuilt
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt GoogleCamera GoogleTTS Photos PixelWallpapers2021 TagGoogle YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
